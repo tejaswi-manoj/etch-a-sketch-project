9fddd3105818d47672ea3f88cec8468829b67e14
Tejaswi Manoj <tejaswimanoj@gmail.com>
2025-06-29 22:05:52 -0400

    Make square appear in the center

---
+++ script.js
@@ -0,0 +1,4 @@
+const container = document.getElementById('container');
+const small_square = document.createElement('div');
+small_square.className = 'square'; 
+container.appendChild(small_square);
