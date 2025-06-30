e5f0a569eb82bfc14be0979fbc54c903b1af6d0f
Tejaswi Manoj <tejaswimanoj@gmail.com>
2025-06-29 22:59:12 -0400

    Calculate correct formula for adjusting grid size and experiment with different number of squares

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const container = document.getElementById('container');
 
-for(let i=0; i<4; i++){
+for(let i=0; i<9; i++){
     const square = document.createElement('div');
     square.className = 'square';
     container.appendChild(square);
