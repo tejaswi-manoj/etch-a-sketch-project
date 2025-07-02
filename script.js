5d1909f7fb1debca992a2bb1da51fe128707d7c0
Tejaswi Manoj <tejaswimanoj@gmail.com>
2025-07-02 02:42:39 -0400

    Fix the grid sizing

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-const container = document.getElementById('container');
+    let userInput = 50;
+    const grid = document.getElementById("grid");
 
-for(let i=0; i<9; i++){
-    const square = document.createElement('div');
-    square.className = 'square';
-    container.appendChild(square);
-}
+    for(let i=0; i<userInput*userInput; i++){
+
+        let tile = document.createElement("div");
+        grid.appendChild(tile);
+        tile.classList.add("tile");
+
+        tile.style.flexBasis = `calc(100% / ${userInput})`;
+    }
+
+
+ 
