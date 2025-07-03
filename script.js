80078a6930107796b6c6ecf77460c92e120b3cd8
Tejaswi Manoj <tejaswimanoj@gmail.com>
2025-07-03 00:11:52 -0400

    Add color input

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-    let userInput = 10;
 
-    const grid = document.getElementById("grid");
 
-    for(let i=0; i<userInput*userInput; i++){
 
-        let tile = document.createElement("div");
-        grid.appendChild(tile);
-        tile.classList.add("tile");
+let userInput = 10;
 
-        tile.style.flexBasis = `calc(100% / ${userInput})`;
-    }    
+const grid = document.getElementById("grid");
+
+for(let i=0; i<userInput*userInput; i++){
+
+    let tile = document.createElement("div");
+    grid.appendChild(tile);
+    tile.classList.add("tile");
+    tile.style.flexBasis = `calc(100% / ${userInput})`;
+}    
+
 
