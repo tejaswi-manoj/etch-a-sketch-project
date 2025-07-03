9fa239d8f4d51d5162da3e7f5a3c81fd7072ed24
Tejaswi Manoj <tejaswimanoj@gmail.com>
2025-07-03 02:24:16 -0400

    Make grid size flexible

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
-document.getElementById("range").addEventListener("input", function(){
-    const value_display  = this.value
-    document.getElementById("value_dis").textContent = `${value_display} x ${value_display}`;
-
-});
-
-
-let userInput = 10;
-
 const grid = document.getElementById("grid");
 
-for(let i=0; i<userInput*userInput; i++){
+function getUserInput(){
+    grid.innerHTML = "";
+    const value = this.value;
+    document.getElementById("value_dis").textContent = `${value} x ${value}`;
+    for(let i=0; i<value*value; i++){
 
     let tile = document.createElement("div");
     grid.appendChild(tile);
     tile.classList.add("tile");
-    tile.style.flexBasis = `calc(100% / ${userInput})`;
+    tile.style.flexBasis = `calc(100% / ${value})`;
 }    
+}
+document.getElementById("range").addEventListener("input", getUserInput);
+
+
+
+
 
 
